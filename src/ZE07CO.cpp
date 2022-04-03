0a5d8e1f0a7b5f36df6a7529d1ecde5abafc11d6
xvrijdag <60059467+xvrijdag@users.noreply.github.com>
2022-04-03 16:28:12 +1200

    Update ZE07CO.cpp
    cleaned functions
    corrected calculation of ppm value
    created functional set mode function and back
    added debugResponse function

--- src/ZE07CO.cpp
+++ src/ZE07CO.cpp
@@ -1,41 +1,46 @@
 #include "Arduino.h"
 #include "ZE07CO.h"
 
-ZE07CO::ZE07CO() {
-_Serial = NULL;
-}
-
-ZE07CO::begin(SoftwareSerial *serialname)
+ZE07CO::ZE07CO()
 {
- _Serial = serialname;
- _Serial->begin(9600);
+  _Serial = NULL;
 }
 
-void ZE07CO::setMode (int m) {
+void ZE07CO::begin(SoftwareSerial *serialname)
+{
+  _Serial = serialname;
+  _Serial->begin(9600);
+}
 
-const byte Start      = 0xFF;
-const byte Sensor     = 0x01;
-const byte Command    = 0x78;
-byte Mode;
-if (m == 1) {
-           Mode       = 0x41;
-         } else {
-           Mode       = 0x40;
-         }
-const byte Zero       = 0x00;
-const byte Checksum   = 0x46;
+void ZE07CO::setMode(int m)
+{
+  const byte Start = 0xFF;
+  const byte Sensor = 0x01;
+  const byte Command = 0x78;
+  byte Mode;
+  byte Checksum;
+  if (m == 1)
+  {
+    Mode = 0x41;
+    Checksum = 0x46;
+  }
+  else
+  {
+    Mode = 0x40;
+    Checksum = 0x47;
+  }
+  const byte Zero = 0x00;
 
-   byte request[9] = {
-    Start,
-    Sensor,
-    Command,
-    Mode,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Checksum
-    };
+  byte request[9] = {
+      Start,
+      Sensor,
+      Command,
+      Mode,
+      Zero,
+      Zero,
+      Zero,
+      Zero,
+      Checksum};
 
   _Serial->write(request, 9);
 
@@ -44,166 +49,91 @@ const byte Checksum   = 0x46;
 
 void ZE07CO::requestCO()
 {
+  const byte Start = 0xFF;
+  const byte Sensor = 0x01;
+  const byte Command = 0x86;
+  const byte Zero = 0x00;
+  const byte Checksum = 0x79;
 
-const byte Start      = 0xFF;
-const byte Sensor     = 0x01;#include "Arduino.h"
-#include "ZE07CO.h"
-
-ZE07CO::ZE07CO(HardwareSerial *Serial)
-{
- _Serial = Serial;
-}
-
-ZE07CO::ZE07CO(SoftwareSerial *Serial)
-{
- _Serial = Serial;
-}
-
-ZE07CO::begin()
-{
- _Serial->begin(9600);
-}
-
-void ZE07CO::setMode (int m) {
-
-const byte Start      = 0xFF;
-const byte Sensor     = 0x01;
-const byte Command    = 0x78;
-byte Mode;
-if (m == 1) {
-           Mode       = 0x41;
-         } else {
-           Mode       = 0x40;
-         }
-const byte Zero       = 0x00;
-const byte Checksum   = 0x46;
-
-   byte request[9] = {
-    Start,
-    Sensor,
-    Command,
-    Mode,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Checksum
-    };
+  byte request[9] = {
+      Start,
+      Sensor,
+      Command,
+      Zero,
+      Zero,
+      Zero,
+      Zero,
+      Zero,
+      Checksum};
 
   _Serial->write(request, 9);
 
   _Serial->flush();
-}
-
-void ZE07CO::requestCO()
-{
-
-const byte Start      = 0xFF;
-const byte Sensor     = 0x01;
-const byte Command    = 0x86;
-const byte Zero       = 0x00;
-const byte Checksum   = 0x79;
-
-   byte request[9] = {
-    Start,
-    Sensor,
-    Command,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Checksum
-    };
-
-  _Serial->write(request, 9);
-
-  _Serial->flush();
-
 }
 
 float ZE07CO::readCO()
 {
 
-  while (_Serial->available() > 0 && _Serial->peek() != 0xFF) {
+  while (_Serial->available() > 0 && _Serial->peek() != 0xFF)
+  {
     _Serial->read();
     delay(1);
   }
 
+  byte response[9];
   _Serial->readBytes(response, 9);
 
   if (response[0] != 0xFF && (response[1] != 0x86 || response[1] != 0x04))
-    {
-      return -1;
-    }
+  {
+    return -1;
+  }
 
   byte checksum = 0;
-  for (int i = 1; i < 8; i++) {
+  for (int i = 1; i < 8; i++)
+  {
     checksum += response[i];
   }
   checksum = 0xFF - checksum;
   checksum++;
 
-   if (response[8] != checksum) {
-     return -2;
-   }
+  if (response[8] != checksum)
+  {
+    return -2;
+  }
+  int lowbit;
+  int highbit;
 
-    int low  = (int)response[4];
-    int high = (int)response[5];
-    float ppm = (high * 256 + low) * 0.1;
-    return ppm;
+  if (response[1] == 0x04)
+  {
+    lowbit = 5;
+    highbit = 4;
+  } else 
+  {
+    lowbit = 3;
+    highbit = 2;
+  }
+  float ppm = ((int)response[highbit] * 256 + (int)response[lowbit]) * 0.1;
+  return ppm;
 }
 
-const byte Command    = 0x86;
-const byte Zero       = 0x00;
-const byte Checksum   = 0x79;
-
-   byte request[9] = {
-    Start,
-    Sensor,
-    Command,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Zero,
-    Checksum
-    };
-
-  _Serial->write(request, 9);
-
-  _Serial->flush();
-
-}
-
-float ZE07CO::readCO()
+void ZE07CO::debugResponse()
 {
 
-  while (_Serial->available() > 0 && _Serial->peek() != 0xFF) {
+  while (_Serial->available() > 0 && _Serial->peek() != 0xFF)
+  {
     _Serial->read();
     delay(1);
   }
 
+  byte response[9];
   _Serial->readBytes(response, 9);
 
-  if (response[0] != 0xFF && (response[1] != 0x86 || response[1] != 0x04))
-    {
-      return -1;
-    }
-
-  byte checksum = 0;
-  for (int i = 1; i < 8; i++) {
-    checksum += response[i];
+  Serial.begin(9600);
+  for (int i=0;i<9;i++)
+  {
+    Serial.print (response[i]);
+    Serial.print (" ");
   }
-  checksum = 0xFF - checksum;
-  checksum++;
-
-   if (response[8] != checksum) {
-     return -2;
-   }
-
-    int low  = (int)response[4];
-    int high = (int)response[5];
-    float ppm = (high * 256 + low) * 0.1;
-    return ppm;
+  Serial.println ("");
+  
 }
