a7e7a6018a9a4b3120fa82ba14eb6450d445e76d
xvrijdag <60059467+xvrijdag@users.noreply.github.com>
2022-04-03 16:29:01 +1200

    Update ZE07CO.h
    cleaned up file
    match cpp file

--- src/ZE07CO.h
+++ src/ZE07CO.h
@@ -8,33 +8,15 @@ class ZE07CO {
   public:
     ZE07CO();
 
-    void begin(SoftwareSerial *serialname);
+    void begin(SoftwareSerial* serialname);
     void setMode(int m = 0);
     void requestCO();
     float readCO();
+    void debugResponse();
 
   private:
     SoftwareSerial *_Serial;
 };
 
 #endif
-#ifndef ZE07CO_h
-#define ZE07CO_h
 
-#include "Arduino.h"
-#include <SoftwareSerial.h>
-
-class ZE07CO {
-  public:
-    ZE07CO(HardwareSerial *Serial);
-    ZE07CO(SoftwareSerial *Serial);
-    
-    void setMode(int m = 0);
-    void requestCO();
-    float readCO();
-
-  private:
-    Stream *_Serial;
-};
-
-#endif
